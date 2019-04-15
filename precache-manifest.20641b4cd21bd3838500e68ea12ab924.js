c43bc513e37ad0d4b3ba0c023c419729b2a67aa5
githiroshi <githiroshi@outlook.com>
2019-04-15 22:02:14 +0900

    Updates

---
+++ precache-manifest.20641b4cd21bd3838500e68ea12ab924.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7a5afec1f45cf8fdc68",
+    "url": "/react-avgle/static/js/runtime~main.1194faf6.js"
+  },
+  {
+    "revision": "426d1c02cd1712156364",
+    "url": "/react-avgle/static/js/main.231ff407.chunk.js"
+  },
+  {
+    "revision": "245653c7f3ad33eebeec",
+    "url": "/react-avgle/static/js/2.7243bebf.chunk.js"
+  },
+  {
+    "revision": "426d1c02cd1712156364",
+    "url": "/react-avgle/static/css/main.4a0dbf81.chunk.css"
+  },
+  {
+    "revision": "a89b96e2eb2f4d7855fee4b646a2c355",
+    "url": "/react-avgle/index.html"
+  }
+];
